e947f5a7296e9f9feed3806bfad3acba2dc28fc7
ttsiligkoudis <themtsil@gmail.com>
2024-10-13 11:25:51 +0300

    commit

--- Helpers/obj/Debug/net7.0/Helpers.AssemblyInfo.cs
+++ Helpers/obj/Debug/net7.0/Helpers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9d0f37b5f88342615539f59ca97523fb78259b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006f6183ab81a74b36e5bffe0a6454a8f022e947")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
